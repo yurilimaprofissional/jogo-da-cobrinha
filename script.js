8d96260c67d606c4b2d5515983b463e50db44152
Yuri Lima <yurilimaprofissional@gmail.com>
2025-05-26 14:38:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
 
-ctx.fillStyle = "red"
+const size = 30
+
+const snake = [
+    {x: 200, y: 200 },
+    {x: 230, y: 200 }
+]
+
+const drawSnake = () => {
+    ctx.fillStyle = "#ddd"
+
+    snake.forEach((position, index) => {
+        if (index == snake.length -1) {
+            ctx.fillStyle = "green"
+        }
+
+        ctx.fillReact(position.x, position.y, size, size)
+    })
+
+}
+
+drawSnake()
